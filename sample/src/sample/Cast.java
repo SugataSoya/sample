ac12d5e9e96412d4ea47f599076f92e85f29e240
SugataSoya <soya.sugata.m512@dirbato.co.jp>
2024-04-05 16:02:48 +0900

    chapter2

---
+++ sample/src/sample/Cast.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Cast {
+
+    public static void main(String[] args) {
+        int score=59;
+        System.out.print("scoreが40以上、かつ50以下：");
+        System.out.println(score >= 40 && score <= 50);
+        System.out.print("scoreが40以上、または50以下：");
+        System.out.println(score >= 40 || score <= 50);
+        System.out.print("scoreが60以上ではない：");
+        System.out.println(!(score >= 60));
+    }
+
+}
